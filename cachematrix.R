3dd406ed7b451a0e0a6d32f404991ed136d670b4
Angel JF <angel.juarez.bqd@gmail.com>
2020-09-17 19:47:03 -0500

    First version of a program that can store a cache of the inverse of a matrix and other to use that cache.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cache store of the computation of the inverse of a matrix
+## 
 
-## Write a short comment describing this function
+## Input of the matrix which can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        #invm = inverse matrix as a empty variable
+        invm <- NULL
+        #asiggn a value to setivm 
+        setivm <- function(y) {
+                x <<- y
+                invm <<- NULL
+        }
+        
+        getivm <- function() x
+        #using solve to obtain the inverse
+        setinvmatrix <- function(solve) invm <<- solve
+        getinvmatrix <- function() invm
+        list(setivm = setivm, getivm = getivm,
+             setinvmatrix = setinvmatrix,
+             getinvmatrix = getinvmatrix)
+        
 }
 
 
-## Write a short comment describing this function
+## Use makecachematrix to compute the inverse of a matrix or not if is stored in cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        #check if there is already data stored for invm
+        invm <- x$getinvmatrix()
+        if(!is.null(invm)) {
+                message("getting cached data")
+                return(invm)
+        }
+        #If not it calculate the inverse
+        data <- x$getivm()
+        invm <- solve(data, ...)
+        
+        ## set and return a matrix that is the inverse of 'x'
+        x$setinvmatrix(invm)
+        invm
 }
